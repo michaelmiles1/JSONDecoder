ad6cd946449b455f5ce7f6225c873b84538b71a6
michaelmiles1 <michael.gage.miles@gmail.com>
2019-11-03 21:18:54 -0800

    Initial Commit

---
+++ JSONDecoder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSONDecoder
+//
+//  Created by Michael Miles on 11/3/19.
+//  Copyright © 2019 Michael Miles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
