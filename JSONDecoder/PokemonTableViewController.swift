927aca61c8f94695032ca9f6bc5d3604ecccb04d
michaelmiles1 <michael.gage.miles@gmail.com>
2019-11-06 15:39:50 -0800

    Changed defaults for tableView.

--- JSONDecoder/PokemonTableViewController.swift
+++ JSONDecoder/PokemonTableViewController.swift
@@ -25,7 +25,7 @@ class PokemonTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 20
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
